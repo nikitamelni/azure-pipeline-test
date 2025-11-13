2557ebc41ec895229550760026d4b1dd5488d61c
nikitam <nikitam@uniform.dev>
2025-11-13 12:47:44 -0500

    turning on the ssg mode with "out" folder as destination for files

--- pages/[[...slug]].tsx
+++ pages/[[...slug]].tsx
@@ -1,10 +1,16 @@
 import PageComposition from "@/components/PageComposition";
-import { withUniformGetServerSideProps } from "@uniformdev/canvas-next/route";
-import { CANVAS_DRAFT_STATE, CANVAS_PUBLISHED_STATE } from "@uniformdev/canvas";
+import {
+  withUniformGetStaticProps,
+  withUniformGetStaticPaths,
+} from "@uniformdev/canvas-next/route";
+import {
+  CANVAS_DRAFT_STATE,
+  CANVAS_PUBLISHED_STATE,
+  RouteClient,
+} from "@uniformdev/canvas";
 import { getCompositionsForNavigation } from "@/lib/uniform/canvasClient";
 
-// IMPORTANT: this starter is using SSR mode by default for simplicity. SSG mode can be enabled, please check Uniform docs here: https://docs.uniform.app/docs/guides/composition/routing#project-map-with-uniform-get-server-side-props-and-with-uniform-get-static-props
-export const getServerSideProps = withUniformGetServerSideProps({
+export const getStaticProps = withUniformGetStaticProps({
   // fetching draft composition in dev mode for convenience
   requestOptions: {
     state:
@@ -12,7 +18,12 @@ export const getServerSideProps = withUniformGetServerSideProps({
         ? CANVAS_DRAFT_STATE
         : CANVAS_PUBLISHED_STATE,
   },
-  handleComposition: async ({ compositionApiResponse }, { preview }, _defaultHandler) => {
+  param: "slug",
+  handleComposition: async (
+    { compositionApiResponse },
+    { preview },
+    _defaultHandler
+  ) => {
     const { composition } = compositionApiResponse || {};
     const navLinks = await getCompositionsForNavigation(preview);
     return {
@@ -23,5 +34,7 @@ export const getServerSideProps = withUniformGetServerSideProps({
     };
   },
 });
+
+export const getStaticPaths = withUniformGetStaticPaths();
 
 export default PageComposition;
