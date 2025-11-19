923bafb0c26c7177f8df87ccf183791a45b05ac8
Eugene <eugene.shulga@dbbsoftware.com>
2025-11-19 14:35:29 +0200

    feat: init project

--- src/fonts/index.ts
+++ src/fonts/index.ts
@@ -1,21 +1,21 @@
-import { DM_Sans, Space_Mono } from 'next/font/google';
+import localFont from 'next/font/local';
 
-export const dm_sans = DM_Sans({
-  subsets: ['latin'],
-  variable: '--dm-sans',
+export const helveticaNeue = localFont({
+  src: [
+    {
+      path: './custom-fonts/HelveticaNeueLTStd-Roman.otf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: './custom-fonts/HelveticaNeueLTStd-Bd.otf',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
+  variable: '--helvetica-neue',
   display: 'swap',
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-  style: ['italic', 'normal'],
   preload: true,
 });
 
-export const space_mono = Space_Mono({
-  subsets: ['latin'],
-  variable: '--space-mono',
-  display: 'swap',
-  weight: ['400', '700'],
-  style: ['italic', 'normal'],
-  preload: true,
-});
-
-export const customFontVariables = [dm_sans.variable, space_mono.variable].join(' ');
+export const customFontVariables = [helveticaNeue.variable].join(' ');
